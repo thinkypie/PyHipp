d6e819373f3136754cc3642dc5e9345646c0b8ad
AWS ParallelCluster user <ec2-user@ip-10-0-1-245.ap-southeast-1.compute.internal>
2022-10-17 10:50:14 +0000

    Update script to generate multiple environments

--- envlist.py
+++ envlist.py
@@ -43,6 +43,10 @@ with lock:
         # create a list (named clist) of nevns environments with the 
         # prefix envprefix
         # add code here
+        clist=[]
+        for  i in range(int(nenvs)):
+            clist.append(envprefix + str(i))	
+        
     else:
         # load hickle file
         clist = hickle.load(file_path)
@@ -50,11 +54,14 @@ with lock:
         if pmode == WRITE_MODE:
             # append item to end of list
             # add code here
+            clist.append(sys.argv[1]) 
+            
         else:    
             # get and remove env from clist
             # add code here
-            # return env name
-            print(env)
+            env=clist.pop(0)
+	    # return env name
+        print(env)
 
     # save hickle file
     hickle.dump(clist, file_path, mode="w")
